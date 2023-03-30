2f6ea446b30f3a279d521fa9be4c45e0e5aa0d1b
barbara <zhujianwang001@gmail.com>
2023-03-30 11:23:03 +0800

    添加noexcept新特性注解

--- c++2.0/c++11/noexcept.cpp
+++ c++2.0/c++11/noexcept.cpp
@@ -6,8 +6,9 @@
 #include <vector>
 
 using namespace std;
-
-// noexcepte ()中可以加条件
+// 该关键字告诉编译器，函数中不会发生异常,这有利于编译器对程序做更多的优化。
+// 如果在运行时，noexecpt函数向外抛出了异常（如果函数内部捕捉了异常并完成处理，这种情况不算抛出异常），程序会直接终止，调用std::terminate()函数，该函数内部会调用std::abort()终止程序。
+// noexcept ()中可以加条件
 // 异常可传递  a调用b b抛出异常,a没处理,就会抛出异常
 // 1.异常的回传机制:调用foo如果抛出异常，foo会接着往上层抛出异常，
 // 如果最上层没有处理，则会调用terminate函数，terminate函数内部调用abort，使程序退出

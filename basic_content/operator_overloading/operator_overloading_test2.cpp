444b60442c6ac09770db0f3161c4596b1e6202d1
barbara <zhujianwang001@gmail.com>
2023-04-13 14:49:42 +0800

    添加for_each注解

---
+++ basic_content/operator_overloading/operator_overloading_test2.cpp
@@ -0,0 +1,35 @@
+
+
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+ 
+
+struct Sum
+{
+    int sum = 0;
+    void operator()(int n) { sum += n; }
+};
+
+ 
+class Foo {
+public:
+    operator double() const {return double(11);}
+    operator std::string() const { return "I am a foo!"; }
+};
+
+
+int main()
+{
+    Foo foo;
+    printf("overloading of operator std::string() %s\n", std::string(foo).c_str()); // Will print "I am a foo!".
+    printf("overloading of operator double() %f\n", double(foo));
+    
+    std::vector<int> v = {1, 2, 3, 4, 5};
+    Sum s = std::for_each(v.begin(), v.end(), Sum());
+    printf("The sum is %d.\n", s.sum);
+    
+    return 0;
+}

462fa9ede2f413bc3525ee9f4a4c824ec33c8e48
barbara <zhujianwang001@gmail.com>
2023-04-13 15:18:44 +0800

    添加按照函数的方式调用重载operator运算符的代码

--- basic_content/operator_overloading/operator_overloading_test2.cpp
+++ basic_content/operator_overloading/operator_overloading_test2.cpp
@@ -24,8 +24,10 @@ public:
 int main()
 {
     Foo foo;
+    printf("overloading of operator std::string() %s\n", foo.operator std::string().c_str()); // Will print "I am a foo!".
     printf("overloading of operator std::string() %s\n", std::string(foo).c_str()); // Will print "I am a foo!".
     printf("overloading of operator double() %f\n", double(foo));
+    printf("overloading of operator double() %f\n", foo.operator double());
     
     std::vector<int> v = {1, 2, 3, 4, 5};
     Sum s = std::for_each(v.begin(), v.end(), Sum());

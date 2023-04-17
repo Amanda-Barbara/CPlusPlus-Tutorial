8f04da51b33b1eb57379ac3ab4628c9585616157
barbara <zhujianwang001@gmail.com>
2023-04-17 17:48:40 +0800

    添加模板特化与偏特化注解

--- design_pattern/CRTP/README.md
+++ design_pattern/CRTP/README.md
@@ -2,8 +2,11 @@
 
 ## [模板特化](./template_specilization.cpp)
 
+## [模板偏特化](./template_partial_specilization.cpp)
+
 ## [实现**静态多态**](./object_counter.cpp)，比如统计派生类对象的个数
 
 
 ## 参考链接
-* 1 [奇异递归模板模式](https://zhuanlan.zhihu.com/p/460497652)
+* 1 [模板特化与偏特化](https://www.jianshu.com/p/4be97bf7a3b9)
+* 2 [奇异递归模板模式](https://zhuanlan.zhihu.com/p/460497652)
--- design_pattern/CRTP/template_specilization.cpp
+++ design_pattern/CRTP/template_specilization.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// 定义Compare类模板
 template <class T>
 class Compare
 {
@@ -8,36 +9,20 @@ public:
      bool IsEqual(const T& arg, const T& arg1);
 };
 
-// 已经不具有template的意思了，已经明确为float了
-template <>
-class Compare<float>
-{
-public:
-     bool IsEqual(const float& arg, const float& arg1);
-};
-
-// 已经不具有template的意思了，已经明确为double了
-template <>
-class Compare<double>
-{
-public:
-     bool IsEqual(const double& arg, const double& arg1);
-};
-
-template <class T>
-bool Compare<T>::IsEqual(const T& arg, const T& arg1)
+template <class T> bool Compare<T>::IsEqual(const T& arg, const T& arg1)
 {
      cout<<"Call Compare<T>::IsEqual"<<endl;
      return (arg == arg1);
 }
 
-bool Compare<float>::IsEqual(const float& arg, const float& arg1)
+// 特例化float类型的
+template <> bool Compare<float>::IsEqual(const float& arg, const float& arg1)
 {
      cout<<"Call Compare<float>::IsEqual"<<endl;
      return (abs(arg - arg1) < 10e-3);
 }
 
-bool Compare<double>::IsEqual(const double& arg, const double& arg1)
+template <> bool Compare<double>::IsEqual(const double& arg, const double& arg1)
 {
      cout<<"Call Compare<double>::IsEqual"<<endl;
      return (abs(arg - arg1) < 10e-6);

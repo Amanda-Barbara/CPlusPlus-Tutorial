eac366ca9780413fdedd5f7806d17fe87ffb51be
barbara <zhujianwang001@gmail.com>
2023-04-17 17:30:02 +0800

    添加CRTP奇异递归模板模式注解

--- design_pattern/CRTP/object_counter.cpp
+++ design_pattern/CRTP/object_counter.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
 
-template <class Derived> 
-struct Base
-{
-    void Interface() {
-        // ...
-        static_cast<Derived*>(this)->Implementation();
-        // ...
-    }
 
-};
+// template <class Derived> 
+// struct Base
+// {
+//     void Interface() {
+//         // ...
+//         static_cast<Derived*>(this)->Implementation();
+//         // ...
+//     }
 
-struct Derived : Base<Derived> {
-    void Implementation();
-};
+// };
 
-template <typename T>
-class derived : public Base<derived<T>> {};
+// struct Derived : Base<Derived> {
+//     void Implementation();
+// };
 
-template <typename T>
-class derived : public Base<derived<int>>{
-  void Implementation(){
-    std::cout << 999 << std::endl;
-  };
-}
+// template <typename T>
+// class derived : public Base<derived<T>> {};
 
 
 template <class Derived>
@@ -47,21 +41,21 @@ class ObjectCounter {
 // class Y : public ObjectCounter<Y> {};
 
 template <typename T>
-class MyVector : public ObjectCounter<MyVector<T>> {}; //声明和定义新类型
+class MyVector: public ObjectCounter<MyVector<T>> {}; //声明和定义新类型
 
 class MyCharString : public ObjectCounter<MyCharString> {}; //声明和定义新类型
 
+template <>
+class MyVector<std::string> : public ObjectCounter<MyVector<std::string>> {}; //声明和定义新类型
+
 int main() {
   MyVector<int> v1, v2;
+  MyVector<std::string> s;
   MyCharString s1;
 
-  std::cout << "number of MyVector<int>: " << MyVector<int>::CountLive()
-            << "\n";
-
+  std::cout << "number of MyVector<int>: " << MyVector<int>::CountLive() << "\n";
+  std::cout << "number of MyVector<std::string>: " << s.CountLive() << "\n";
   std::cout << "number of MyCharString: " << MyCharString::CountLive() << "\n";
-
-
-  derived<int>
 
   return 0;
 }
